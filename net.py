628cf8112a9bde39d17beae87260795ec426841c
kehlerr <hexlite24@gmail.com>
2018-03-11 23:10:33 +0300

    добавлен кроп имги в шаблоне; net в объект

--- net.py
+++ net.py
@@ -2,30 +2,14 @@ import pygame
 from PIL import Image
 from field_defs import *
 from color_defs import SNOW
+import obj_template
 
 
-class Net(pygame.sprite.Sprite):
-    def __init__(self, sfce, pos_x, pos_y, width, height):
-        pygame.sprite.Sprite.__init__(self)
-        self.sfce = sfce
-        img = Image.open(r'pic/net5.png').crop((0,0, width, height))
-        cropped_imgname = r'pic/tmp_net.png'
-
-        try:
-            cropped_imgfile = open(cropped_imgname, 'wb')
-            img.save(cropped_imgfile, 'png', quality=90)
-            cropped_imgfile.close()
-        except:
-            print 'Cannot save temp image'
-
-        try:
-            self.image = pygame.image.load(cropped_imgname)
-            self.sfce.blit(self.image, (pos_x, pos_y))
-            self.rect  = self.image.get_rect()
-            self.rect.move_ip(pos_x, pos_y)
-        except:
-             print 'Cannot open image'
-        finally:
+class Net(obj_template.T):
+    def __init__(self, spr_img, size, pos, sfce, crop_rect):
+        obj_template.T.__init__(self, spr_img, size, pos, sfce=sfce, crop_rect=crop_rect)
+'''
             for y in range(FIELD_H / NET_DENSE):
                 pygame.draw.aaline(self.sfce, SNOW, (pos_x, y*NET_DENSE), (pos_x+NET_SIZE, (y+1)*NET_DENSE), 1)
                 pygame.draw.aaline(self.sfce, SNOW, (pos_x+NET_SIZE, y*NET_DENSE), (pos_x, (y+1)*NET_DENSE), 1)
+'''
--- obj_template.py
+++ obj_template.py
@@ -5,11 +5,9 @@ import pygame
 from common import X, Y
 #from color_defs import WHITE
 
-# TODO [low]: добавить кроп имги
-
 
 class T(pygame.sprite.Sprite):
-    def __init__(self, spr_img, size, pos, group=None, sfce=None):
+    def __init__(self, spr_img, size, pos, group=None, sfce=None, crop_rect=None):
         pygame.sprite.Sprite.__init__(self)
 
         if group is not None:
@@ -17,8 +15,11 @@ class T(pygame.sprite.Sprite):
 
         self.sfce = sfce
         self.size = min(size)
-#       TODO: handle exception here if img not found
-        self.image = pygame.image.load(spr_img)
+        try:
+            self.image = pygame.image.load(spr_img)
+        except pygame.error:
+# TODO: [medium] метод для отрисовки дефолтных примитивов
+            print 'Error!'
         if size[X] is not 0:
             width = size[X]
             height = size[Y] if len(size) > 1 else size[X]
@@ -34,7 +35,7 @@ class T(pygame.sprite.Sprite):
         self.put(pos=pos)
         if sfce is not None:
             self.sfce = sfce
-            self.sfce.blit(self.image, self.rect)
+            self.sfce.blit(self.image, self.rect, crop_rect)
 
     def __del__(self):
         self.kill()
